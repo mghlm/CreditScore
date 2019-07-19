46eed55efee4bbbd4525588b799b27fca8360656
mghlm <mail@magnusholm.com>
2019-07-19 10:55:05 +0100

    Setup models

---
+++ CreditScore/Home/HomeScreenViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeScreenViewController.swift
+//  CreditScore
+//
+//  Created by Magnus Holm on 19/07/2019.
+//  Copyright © 2019 Magnus Holm. All rights reserved.
+//
+
+import Foundation
